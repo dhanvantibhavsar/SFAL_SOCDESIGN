447f4ee641231869032049f5f53275e81b9786ad
dhanvantibhavsar <dhanvantibhavsar98@gmail.com>
2025-01-11 13:38:54 +0000

    added make file for gls

--- caraval_scl/caravel_scl180/synthesis/synth.tcl
+++ caraval_scl/caravel_scl180/synthesis/synth.tcl
@@ -27,16 +27,16 @@ read_file $verilog_files/defines.v
 read_file $verilog_files -autoread -define USE_POWER_PINS -format verilog -top $top_module
 
 
-#elaborate $top_module
-#link
-#read_sdc "$root_dir/synthesis/caravel.sdc"
-#compile_ultra
+elaborate $top_module
+link
+read_sdc "$root_dir/synthesis/caravel.sdc"
+compile_ultra
 
-#report_qor > "$report_dir/qor_post_synth.rpt"
-#report_area > "$report_dir/area_post_synth.rpt"
-#report_power > "$report_dir/power_post_synth.rpt"
+report_qor > "$report_dir/qor_post_synth.rpt"
+report_area > "$report_dir/area_post_synth.rpt"
+report_power > "$report_dir/power_post_synth.rpt"
 
-#write -format verilog -hierarchy -output $output_file
+write -format verilog -hierarchy -output $output_file
 
 
 
