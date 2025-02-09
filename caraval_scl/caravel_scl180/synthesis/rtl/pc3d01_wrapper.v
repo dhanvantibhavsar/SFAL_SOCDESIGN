8cafeed3bbfbccf3a01d00d8526b159dcbd62522
dhanvantibhavsar <dhanvantibhavsar98@gmail.com>
2025-02-09 18:19:02 +0530

    Updated readme file

--- caraval_scl/caravel_scl180/synthesis/rtl/pc3d01_wrapper.v
+++
@@ -1,8 +0,0 @@
-`include "/home/dhanvanti/vsd_sfal/SFAL_SOCDESIGN/caraval_scl/caravel_scl180/synthesis/rtl/pc3d01.v"
-module pc3d01_wrapper(output IN, input PAD);
-
-         pc3d01 pad ( 
-	 .CIN(IN), 
-	 .PAD(PAD)
-          );
-endmodule
--- caraval_scl/caravel_scl180/synthesis/rtl/pt3b02_wrapper.v
+++
@@ -1,5 +0,0 @@
-`include "/home/dhanvanti/vsd_sfal/SFAL_SOCDESIGN/caraval_scl/caravel_scl180/synthesis/rtl/pt3b02.v"
-module pt3b02_wrapper(output IN, inout PAD, input OE_N);
-pt3b02 pad(.CIN(IN), .OEN(OE_N), .I(), .PAD(PAD));
-
-endmodule
